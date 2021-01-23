0d7f8afabdb4db98cc9538ba778b27b9c7851752
Mond.al <mond.al@kakaocorp.com>
2021-01-23 20:53:02 +0900

    updating site 202101232053

--- push.sh
+++
@@ -1,8 +0,0 @@
-timestamp=`date +%Y%m%d%H%M`
-
-bundle exec jekyll build
-cd output
-git add --all .
-git commit -m "updating site $timestamp"
-git push https://github.com/mond-al/mond-al.github.io
-cd ..
