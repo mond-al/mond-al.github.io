84c25dbff73bbeeaf885ad290d72064b166d819b
Mond.al <mond.al@kakaocorp.com>
2021-01-28 20:53:28 +0900

    updating site 202101282053 rollback_default_skin

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,11 @@
-// var gulp = require('gulp');
-//
-// // gulp plugins and utils
-// var gutil = require('gulp-util');
-// var livereload = require('gulp-livereload');
-// var postcss = require('gulp-postcss');
-// var sourcemaps = require('gulp-sourcemaps');
-// var zip = require('gulp-zip');
-//
-// // postcss plugins
-// var autoprefixer = require('autoprefixer');
-// var colorFunction = require('postcss-color-function');
-// var cssnano = require('cssnano');
-// var customProperties = require('postcss-custom-properties');
-// var easyimport = require('postcss-easy-import');
-//
-// var swallowError = function swallowError(error) {
-//     gutil.log(error.toString());
-//     gutil.beep();
-//     this.emit('end');
-// };
-//
-// var nodemonServerInit = function () {
-//     livereload.listen(1234);
-// };
-//
-// gulp.task('build', ['css'], function (/* cb */) {
-//     return nodemonServerInit();
-// });
-//
-// gulp.task('css', function () {
-//     var processors = [
-//         easyimport,
-//         customProperties,
-//         colorFunction(),
-//         autoprefixer({browsers: ['last 2 versions']}),
-//         cssnano()
-//     ];
-//
-//     return gulp.src('assets/css/*.css')
-//         .on('error', swallowError)
-//         .pipe(sourcemaps.init())
-//         .pipe(postcss(processors))
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest('assets/built/'))
-//         .pipe(livereload());
-// });
-//
-// gulp.task('watch', function () {
-//     gulp.watch('assets/css/**', ['css']);
-// });
-//
-// gulp.task('zip', ['css'], function() {
-//     var targetDir = 'dist/';
-//     var themeName = require('./package.json').name;
-//     var filename = themeName + '.zip';
-//
-//     return gulp.src([
-//         '**',
-//         '!node_modules', '!node_modules/**',
-//         '!dist', '!dist/**'
-//     ])
-//         .pipe(zip(filename))
-//         .pipe(gulp.dest(targetDir));
-// });
-//
-// gulp.task('default', ['build'], function () {
-//     gulp.start('watch');
-// });
-
-
 var gulp = require('gulp');
 
 // gulp plugins and utils
 var gutil = require('gulp-util');
+var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
+var zip = require('gulp-zip');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -84,10 +14,18 @@ var cssnano = require('cssnano');
 var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
 
-gulp.task('images', function() {
-    return gulp.src('assets/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('assets/built/images/'))
+var swallowError = function swallowError(error) {
+    gutil.log(error.toString());
+    gutil.beep();
+    this.emit('end');
+};
+
+var nodemonServerInit = function () {
+    livereload.listen(1234);
+};
+
+gulp.task('build', ['css'], function (/* cb */) {
+    return nodemonServerInit();
 });
 
 gulp.task('css', function () {
@@ -100,8 +38,32 @@ gulp.task('css', function () {
     ];
 
     return gulp.src('assets/css/*.css')
+        .on('error', swallowError)
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('assets/built/'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/css/**', ['css']);
+});
+
+gulp.task('zip', ['css'], function() {
+    var targetDir = 'dist/';
+    var themeName = require('./package.json').name;
+    var filename = themeName + '.zip';
+
+    return gulp.src([
+        '**',
+        '!node_modules', '!node_modules/**',
+        '!dist', '!dist/**'
+    ])
+        .pipe(zip(filename))
+        .pipe(gulp.dest(targetDir));
+});
+
+gulp.task('default', ['build'], function () {
+    gulp.start('watch');
 });
