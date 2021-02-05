f06baaf3eb01275b1b9c3f119ca787e8acac6b12
Mond.al <mond.al@kakaocorp.com>
2021-02-06 02:54:52 +0900

    updating site 202102060254

---
+++ push.sh
@@ -0,0 +1,14 @@
+timestamp=`date +%Y%m%d%H%M`
+echo "-------------------------------------------------source upload [1] -------------------------------------------------"
+git add --all .
+git commit -m "updating site $timestamp ${1}"
+git push
+echo "-------------------------------------------------source compile [2]-------------------------------------------------"
+bundle exec jekyll build --incremental
+cd ../mond-al.github.io
+git add --all .
+git commit -m "updating site $timestamp ${1}"
+echo "-------------------------------------------------ouput upload Strart-------------------------------------------------"
+git push https://github.com/mond-al/mond-al.github.io
+cd ../mond-al.github.io_maker/
+./update.sh
