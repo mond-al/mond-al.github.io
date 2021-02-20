375d71bf9b52f1049c2a0a9dbad4b63b5a62b07e
Mond.al <mond.al@kakaocorp.com>
2021-02-20 20:41:04 +0900

    updating site 202102202040

--- assets/images/makeCover.py
+++ assets/images/makeCover.py
@@ -6,59 +6,66 @@ from PIL import ImageFont
 from PIL import ImageDraw
 from PIL import Image
 
+apiMode = False
 
+folderName = 'cover'
 width = 800
 height = 540
 pageLimit = 10
-textSizeFat = 1.2
-textSoruce = "FragmentPagerAdapter"
-query = "desk, work"
+textSizeMultiple = 1
+textSoruce = "Android develop"
+query = "dog"
 
-def linkFetch():
+
+def linkByApi():
     page = random.randint(1, pageLimit)
     url = f"https://api.unsplash.com/search/photos?fm=png&page={page}&query={query}&client_id=yzDacpHm8YTM53AXHxeFh_PwnCFPfPlzrCgI2TWhX1Y"
-    print(url)
-    response_ = requests.get(url)
-    results_ = response_.json()["results"]
-    limit = len(results_)
-    rand = random.randint(1, int(limit))
-    full_ = results_[rand]["urls"]["full"]
-    data = full_ + f"&w={width}&h={height}&fit=crop"
-    print(f"data:{data}")
+    apiResponse = requests.get(url)
+    resultObj = apiResponse.json()["results"]
+    randomLimit = len(resultObj)
+    rand = random.randint(1, int(randomLimit))
+    sourceUrl = resultObj[rand]["urls"]["full"]
+    data = sourceUrl + f"&w={width}&h={height}&fit=crop"
     return data
 
 
-def link2():
+def linkByUrl():
     return f"https://source.unsplash.com/{width}x{height}/?{query}"
 
 
-folderName = 'cover'
-fontFileName = 'NotoSansKR-Bold.otf'
-tempFileName = "temp.jpg"
-tempFileOutput = f"{folderName}/{tempFileName}"
-fontFile = f"font/{fontFileName}"
+def setUrl():
+    if apiMode:
+        img_url = linkByApi()
+    else:
+        img_url = linkByUrl()
+    return img_url
 
 
 def genFile(retryCount=0):
+    tempFileName = "temp.jpg"
+    tempFileOutput = f"{folderName}/{tempFileName}"
+    fontFileName = 'NotoSansKR-Bold.otf'
+    fontFile = f"font/{fontFileName}"
     try:
         print(f"------ > {retryCount}")
         if retryCount >= 10:
             return "None"
-        img_url = link2()  # linkFetch()
+        img_url = setUrl()
+        print(img_url)
+
         response = requests.get(img_url)
         BytesIO(response.content)
-        g = (BytesIO(response.content))
+        sourceImage = (BytesIO(response.content))
         with open(tempFileOutput, 'wb') as out:
-            out.write(g.read())
+            out.write(sourceImage.read())
         im = Image.open(tempFileOutput)
-        textSize = int(width / (len(textSoruce)) * (textSizeFat))
+        textSize = int(width / (len(textSoruce)) * (textSizeMultiple))
         font = ImageFont.truetype(fontFile, textSize)
         draw = ImageDraw.Draw(im)
         textWidth, textHeight = draw.textsize(textSoruce, font)
         textX = ((width - textWidth) / 2)
         textY = ((height - textHeight) / 2)
         draw.text((textX, textY), textSoruce, (255, 255, 255), font)
-        #outputFile: str = folderName + f"/_{query}_" + textSoruce + ".png"
         outputFile: str = folderName + f"/_new.png"
         im.save(outputFile, format="JPEG", quality=82, optimize=True)
         os.remove(tempFileOutput)
@@ -74,3 +81,4 @@ else:
     join = os.path.join(os.path.dirname(__file__), filePath)
     print(join)
     open(join)
+
