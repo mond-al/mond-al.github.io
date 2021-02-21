9fb81f09a1ce05a889ee9c5e703bb64216900ae6
Mond.al <mond.al@kakaocorp.com>
2021-02-21 11:26:16 +0900

    updating site 202102211126

---
+++ requestGoogleUpdate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import webbrowser
+
+"""
+Little script that replaces comment characters to html,
+found in the *.hbs files.
+"""
+
+import os
+import requests
+
+url = "http://www.google.com/ping?sitemap=https://mond-al.github.io/sitemap.xml"
+fileName = "requestResult.html"
+if os.path.exists(fileName):
+    os.remove(fileName)
+f = open(fileName, mode='wt')
+
+res = requests.get(url)
+f.write(res.text)
+print(res.status_code)
+webbrowser.open_new_tab(fileName)
